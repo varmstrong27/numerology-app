2d9ad21d61c3a4f2ae29386a9c9d41bcefb80691
Veronica Armstrong <varmstrong27@gmail.com>
2015-11-11 13:47:44 -0800

    second full file commit

--- spec/people_spec.rb
+++ spec/people_spec.rb
@@ -3,8 +3,16 @@ require 'spec_helper'
 describe 'Our Person Class' do
   include SpecHelper
 
+  before (:all) do
+    @person = Person.create(first_name: "Miss", last_name: "Piggy", birthdate: DateTime.now - 40.years )
+  end
+  
+  after (:all) do
+    @person.delete
+  end
+  
   it 'creates a Person' do
-    person = Person.create(first_name: "Miss", last_name: "Piggy", birthdate: DateTime.now - 40.years )
+    person = Person.create(first_name: "Sample", last_name: "Person", birthdate: (DateTime.now - 25.years))
     expect(person.present?).to be(true)
     person.delete
   end
